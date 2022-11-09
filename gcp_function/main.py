dc20b16b776c57764dceafdfcfd60f62ca7c577f
Mike Trifonov <mike.trifonov@gmail.com>
2022-11-09 16:08:52 +0300

    cosmetics of python code

--- gcp_function/main.py
+++ gcp_function/main.py
@@ -1,10 +1,6 @@
 import base64
 
-def main(event, context): #noqa
-    """Triggered from a message on a Cloud Pub/Sub topic.
-    Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
-    """
-    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-    print(pubsub_message)
+
+def main(event, context): # noqa
+
+    print('here is the test message which appears in logs explorer')
