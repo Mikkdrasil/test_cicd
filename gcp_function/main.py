e2117a86f346f0b7cb17d30c4fc925a0f89f4ef5
Mike Trifonov <mike.trifonov@gmail.com>
2022-11-20 17:14:14 +0100

    combined two branches: main with description and local_update with print 2222

--- gcp_function/main.py
+++ gcp_function/main.py
@@ -5,3 +5,5 @@ def main(event, context): # noqa
     """it is a description of what is happening in this function"""
     
     print('here is the test message which appears in logs explorer')
+
+    print('2222 here is the test message which appears in logs explorer')
