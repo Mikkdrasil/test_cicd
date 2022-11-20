ac0e6d98d226ac8d809c19b27786137e4323f524
Mikkdrasil <79911692+Mikkdrasil@users.noreply.github.com>
2022-11-20 17:12:41 +0100

    Update main.py

--- gcp_function/main.py
+++ gcp_function/main.py
@@ -2,5 +2,6 @@ import base64
 
 
 def main(event, context): # noqa
-
+    """it is a description of what is happening in this function"""
+    
     print('here is the test message which appears in logs explorer')
