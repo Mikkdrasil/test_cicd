24a139993c7a297526d95181cb8737cc841b5af2
Mike Trifonov <mike.trifonov@gmail.com>
2022-11-23 20:23:15 +0100

    changed repo name

--- new_directory/gcp_function/main.py
+++ new_directory/gcp_function/main.py
@@ -4,4 +4,4 @@ import base64
 def main(event, context): # noqa
     """it is a description of what is happening in this function"""
     
-    print('in a new folder now')
+    print('repository renamed')
