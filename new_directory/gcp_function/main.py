8f85ae297a98b5ed87884e55682784e9d4e7bfcb
Mike Trifonov <mike.trifonov@gmail.com>
2022-11-23 20:12:18 +0100

    testing change of folders

--- new_directory/gcp_function/main.py
+++ new_directory/gcp_function/main.py
@@ -4,6 +4,4 @@ import base64
 def main(event, context): # noqa
     """it is a description of what is happening in this function"""
     
-    print('it is a comment from contributor')
-
-    print('2222 here is the test message which appears in logs explorer')
+    print('in a new folder now')
